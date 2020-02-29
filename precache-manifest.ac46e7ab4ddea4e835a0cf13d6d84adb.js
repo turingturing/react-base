216bab65669f8e628e95586e5008ad817157124a
Prabhakhar Kaliyamurthy <pkaliyamurthy@paypal.com>
2020-02-29 03:22:24 +0000

    Updates

---
+++ precache-manifest.ac46e7ab4ddea4e835a0cf13d6d84adb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54c112e7b28e893334341a7c7d2d266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa518e91b7d10da5fe3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef489d1f16e2593a205e",
+    "url": "/static/js/2.0f2fbd60.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0f2fbd60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa518e91b7d10da5fe3",
+    "url": "/static/js/main.b43db362.chunk.js"
+  },
+  {
+    "revision": "e2be6e90182613c59673",
+    "url": "/static/js/runtime-main.36c0ecf6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
